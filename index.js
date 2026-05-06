92bd531bc95645c82729d8c74c6b79d17ad3d9fa
rain1201 <rain1201@yeah.net>
2026-05-06 18:03:43 +0800

    rct

---
+++ index.js
@@ -0,0 +1,2 @@
+// 仅仅为了满足 Vercel 的构建要求
+export default () => new Response("NextDNS Proxy is running.");
--- main.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req) {
-    return new Response('Hello World', {
-        status: 200,
-        headers: {
-            'Content-Type': 'text/plain',
-        },
-    });
-}
